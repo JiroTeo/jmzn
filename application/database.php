ad84f6eb46b810e90ac5ac4535216c9e9b9764b0
wyz <18231861418@163.com>
2019-08-21 17:53:29 +0800

    提交代码

--- application/database.php
+++ application/database.php
@@ -13,14 +13,14 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-//    'hostname'        => '47.94.217.131',
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '47.94.217.131',
+//    'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'inv',
+    'database'        => 'jmzn',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '',
+    'password'        => 'maimai!@#456',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
@@ -30,7 +30,7 @@ return [
     // 数据库编码默认采用utf8
     'charset'         => 'utf8',
     // 数据库表前缀
-    'prefix'          => 'inv_',
+    'prefix'          => 'jm_',
     // 数据库调试模式
     'debug'           => false,
     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
